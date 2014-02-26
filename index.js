4a9b04b9d1390acd2512e4d9e66d5002ed992f46
Egor <sapronov.egor@gmail.com>
2014-02-26 21:43:39 +0700

    Small refactoring.

--- index.js
+++ index.js
@@ -1,61 +1,41 @@
-var alphabet=function(){
-	return ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',''];
+var alphabet = function () {
+    return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ''];
 };
 
-var crypt = function(message, key, aplh){
-	var tempKey='';
-
-	while(tempKey.length<=message.length){
-		tempKey=tempKey+key;
-	};
-
-	tempKey=tempKey.slice(0,message.length);
-
-	var tempAlph=aplh();
-
-	var result='';
-
-	for (var i = 0; i < message.length; i++) {
-		var Pi=tempAlph.indexOf(message[i]);
-		var Ki=tempAlph.indexOf(tempKey[i]);
-
-		var keyI=(Pi + Ki) % 27;
-
-		result=result+tempAlph[keyI];
-	};
-
-	console.log(result);
-
-	return result;
+var crypt = function (c, k, length) {
+    return (c + k) % length;
 };
 
-var decrypt = function(message, key, aplh){
-	var tempKey='';
-
-	while(tempKey.length<=message.length){
-		tempKey=tempKey+key;
-	};
-
-	tempKey=tempKey.slice(0,message.length);
-
-	var tempAlph=aplh();
-
-	var result='';
-
-	for (var i = 0; i < message.length; i++) {
-		var Ci=tempAlph.indexOf(message[i]);
-		var Ki=tempAlph.indexOf(tempKey[i]);
-
-		var keyI=(Ci-Ki +27) % 27;
-
-		result=result+tempAlph[keyI];
-	};
-
-	console.log(result);
+var decrypt = function (c, k, length) {
+    return (c - k + length) % length;
 };
 
-var result = crypt('attackatdawn','lemon',alphabet);
+var run = function (message, key, aplh, action) {
+    var tempKey = '';
 
-decrypt(result,'lemon',alphabet);
+    while (tempKey.length <= message.length) {
+        tempKey = tempKey + key;
+    }
+
+    tempKey = tempKey.slice(0, message.length);
+
+    var result = '';
+
+    for (var i = 0; i < message.length; i++) {
+        var c = aplh.indexOf(message[i]);
+        var k = aplh.indexOf(tempKey[i]);
+
+        var keyI = action(c, k, aplh.length);
+
+        result = result + aplh[keyI];
+    }
+
+    return result;
+};
+
+var result = run('attackatdawn', 'lemon', alphabet(), crypt);
+console.log(result);
+result = run(result, 'lemon', alphabet(), decrypt);
+console.log(result);
 
 
