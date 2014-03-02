3f027d267f9c73bfc48992595c56c8878d14d9ee
Egor <sapronov.egor@gmail.com>
2014-03-02 15:44:13 +0700

    Complete modified version.

---
+++ vigenery-modified.js
@@ -0,0 +1,26 @@
+var vigeneryModified = {
+    keyWord: String,
+
+    execute: function (message, callback) {
+        var key = this.keyWord;
+        while (key.length <= message.length) {
+            key += this.keyWord;
+        }
+
+        var messageBytes = message.getBytes();
+        var keyBytes = key.getBytes();
+
+        var result = '';
+        for (var i = 0; i < message.length; i++) {
+            var currCharCode = messageBytes[i] ^ keyBytes[i];
+            var newChar = String.fromCharCode(currCharCode);
+            result =result+ newChar;
+        }
+
+        if (typeof callback == 'undefined') {
+            return result;
+        } else {
+            return callback(result);
+        }
+    }
+};
